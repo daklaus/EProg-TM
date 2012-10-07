39a72e1c0ad52345c8346e0b26a3145d9683c372
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2012-10-07 17:49:23 +0200

    Finished exercise 1.1 to 1.3

---
+++ serie01/kreis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+	double r=0, h=0, area=0, volume=0;
+	
+	printf("Insert radius: ");
+	scanf("%lf", &r);
+	printf("Insert height: ");
+	scanf("%lf", &h);
+	
+	area = M_PI * r * r;
+	printf("Area of the circle: A=%f\n", area);
+
+	if(h == 0)
+		return EXIT_SUCCESS;
+
+	volume = area * h / 3;
+	printf("Volume of the cone: V=%f\n", volume);
+
+	return EXIT_SUCCESS;
+}
